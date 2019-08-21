f73d14c2018ab315e63d920400d4c8e701d566bc
Jonathan Massuchetti <jonathan.massuchetti@xxii.fr>
2019-08-21 22:48:06 +0200

    remove unnecessary statements

--- transaction-faucet.js
+++ transaction-faucet.js
@@ -8,14 +8,6 @@ class FaucetTransaction extends BaseTransaction {
         return 777;
     }
 
-    /**
-     * Set the `CashbackTransaction` transaction FEE to 0.1 LSK.
-     * Every time a user posts a transaction to the network, the transaction fee is paid to the delegate who includes the transaction into a block that the delegate forges.
-     */
-    static get FEE () {
-        return 0;
-    };
-
     validateAsset() {
         return [];
     }
@@ -24,10 +16,6 @@ class FaucetTransaction extends BaseTransaction {
         await store.account.cache([ { address: this.recipientId } ]);
     }
 
-    /**
-     * The CashbackTransaction adds an inflationary 10% to senders account.
-     * Invoked as part of the apply() step of the BaseTransaction and block processing.
-     */
     applyAsset(store) {
 
         const recipient = store.account.get(this.recipientId);
@@ -40,10 +28,6 @@ class FaucetTransaction extends BaseTransaction {
         return [];
     }
 
-    /**
-     * Inverse of applyAsset().
-     * Undoes the changes made in `applyAsset` step: It sends the transaction amount back to the sender and substracts 10% of the transaction amount from the senders account balance.
-     */
     undoAsset(store) {
 
         const recipient = store.account.get(this.recipientId);
